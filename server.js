5b5efe1c16580ba0f0b7c3f5e8fa1a140a46a826
Ethan Seng <eth.sengsavang@gmail.com>
2020-03-14 12:47:50 -0600

    Create getIndex and getStyles functions

--- server.js
+++ server.js
@@ -8,8 +8,31 @@ function error404(request){
     return;
 }
 
-function onRequest(request, response){
+function getIndex(response){
+    fs.readFile("./index.html", function(error, data){
+        response.writeHead(200, {"ContentType" : "text/html"});
+        response.write(data);
+        response.end();
+    });
+}
 
+function getStyles(response){
+    fs.readFile("./css/styles.css", function(error, data){
+        response.writeHead(200, {"ContentType" : "text/css"});
+        response.write(data);
+        response.end();
+    });
+}
+
+function onRequest(request, response){
+    console.log("A request has been made to %s", request.url);
+
+    if(request.method == "GET" && request.url == "/"){
+        getIndex(response);
+    }
+    if(request.method){
+        getStyles(response);
+    }
 }
 
 http.createServer(onRequest).listen(port);
