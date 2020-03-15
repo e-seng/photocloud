1409a6dfa6c8a113d7f3169d2af278fd31f5eb49
Ethan Seng <eth.sengsavang@gmail.com>
2020-03-15 12:59:06 -0600

    Revert server.js

--- server.js
+++ server.js
@@ -22,8 +22,8 @@ function error404(response){
 }
 
 function getIndex(response){
-    fs.readFile("./index.php", function(error, data){
-        response.writeHead(200, {"ContentType" : "text/php"});
+    fs.readFile("./index.html", function(error, data){
+        response.writeHead(200, {"ContentType" : "text/html"});
         response.write(data);
         response.end();
     });
