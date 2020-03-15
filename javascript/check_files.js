d229f44cd7fdf4e4552d7d626709655c576846a3
Ethan Seng <eth.sengsavang@gmail.com>
2020-03-14 21:11:14 -0600

    Create string format function

--- javascript/check_files.js
+++ javascript/check_files.js
@@ -1,3 +1,17 @@
 // Checks for and populates the website with photos found within the local directory
+var fs = require("fs");
 
-document.getElementById("").innerHTML = "";
+String.prototype.format = function(){
+    let string = this;
+
+    for (let index in arguments){
+        string = string.replace("{" + addText + "}", arugments[index]);
+    }
+
+    return string;
+}
+
+var files = fs.readdirSync("./stockimages/");
+console.log(files)
+
+//document.getElementById("").innerHTML = "";
