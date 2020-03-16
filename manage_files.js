1beabba665df53eb16afdbb4b481e7c2e7a98556
Ethan Seng <eth.sengsavang@gmail.com>
2020-03-15 23:41:07 -0600

    Determine which files are recently uploaded

--- manage_files.js
+++ manage_files.js
@@ -3,12 +3,59 @@ var fs = require("fs");
 
 const DIRECTORIES = ["./stockimages/",]
 
+function getFileNames(){
+    let files = [];
+    DIRECTORIES.forEach(function(dir){
+        filenames = fs.readdirSync(dir);
+        
+        filenames.forEach(function(file){
+            let filepath = "{0}{1}\n".format(dir, file);
+            files.push(filepath);
+        });
+    });
+    return files;
+}
+
+function getExistingFiles(reader, array){
+    reader.on("data", function(data){
+        func(data);
+    });
+
+    reader.on("end", function(){
+        console.log("end");
+    });
+}
+
+function dump_data(data, array){
+    array.push(data);
+}
+
 module.exports = {
     updateJson: function updateJson(){
-        DIRECTORIES.forEach(function(filepath){
-            let files = fs.readdirSync(filepath);
-            console.log(files)
+
+        let files = getFileNames();
+        let existing_files = fs.readFileSync("photo_list.txt", "utf-8").split("\n");
+
+
+        files.forEach(function(file){
+            file = file.toString();
+            if(existing_files.includes(file.split("\n")[0])){return;}
+            
+            fs.appendFile("./photo_list.txt", file, function(err){
+                if(err) throw err;
+            });   
+            
         });
     } 
+}
+
+String.prototype.format = function(){
+    let string = this;
+
+    for (let index in arguments){
+        string = string.replace("{" + addText + "}", arugments[index]);
+    }
+
+    return string;
 }
 
