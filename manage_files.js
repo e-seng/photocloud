9804c563575a2a97988dac9b3774ba4726fb0350
Ethan Seng <eth.sengsavang@gmail.com>
2020-03-18 10:30:06 -0600

    Send the number of files necessary to the server

--- manage_files.js
+++ manage_files.js
@@ -23,7 +23,6 @@ module.exports = {
         let files = getFileNames();
         let existing_files = fs.readFileSync("photo_list.txt", "utf-8").split("\n");
 
-
         files.forEach(function(file){
             file = file.toString();
             if(existing_files.includes(file.split("\n")[0])){return;}
@@ -33,7 +32,26 @@ module.exports = {
             });   
             
         });
-    } 
+    },
+
+    getFiles: function getFiles(desiredAmount, currentCount){
+        let existingFiles = fs.readFileSync("photo_list.txt", "utf-8").split("\n");
+        let limit = existingFiles.length - 1;
+        let finalString = "{0}";
+
+        for(let counter = 0; counter < desiredAmount; counter++){
+            let index = currentCount + counter;
+            if(index >= limit){break;}
+
+            let photoURL = existingFiles[index];
+            console.log(photoURL)
+            let addString = "<img src={0} class=\"photo\" />{1}".format(photoURL,"{0}");
+            finalString = finalString.format(addString);
+        }
+
+        finalString = finalString.format("\n");
+        return finalString;
+    }
 }
 
 String.prototype.format = function(){
