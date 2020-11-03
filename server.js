04940288b33fb76919bfd5699b203c22335a5999
e-seng <eth.sengsavang@gmail.com>
2020-11-03 14:08:55 -0700

    Fix getFileType return

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const ROOT_DIR = "."
 
 function getFileType(request){
     let filename = request.url;
-    return filename.split('.').slice(-1);
+    return filename.split('.').slice(-1)[0];
 }
 
 function error404(response){
